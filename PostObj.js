85fade2c0d0dd81b9986d8581b5eca25243f9619
kwamina <atokop@gmail.com>
2013-09-22 02:10:42 -0400

    fixed EVERYTHING (that's not true)

--- PostObj.js
+++ PostObj.js
@@ -22,12 +22,12 @@ PostObj.set("message", message);
 PostObj.save(null, {
   success: function(PostObj) {
     // Execute any logic that should take place after the object is saved.
-    alert('New object created with objectId: ' + PostObj.id);
+    //alert('New object created with objectId: ' + PostObj.id);
   },
   error: function(PostObj, error) {
     // Execute any logic that should take place if the save fails.
     // error is a Parse.Error with an error code and description.
-    alert('Failed to create new object, with error code: ' + error.description);
+    //alert('Failed to create new object, with error code: ' + error.description);
   }
 });
 
--- TestObject.js
+++ TestObject.js
@@ -7,7 +7,7 @@ var TestObject = Parse.Object.extend("TestObject");
 var testObject = new TestObject();
 testObject.save({foo: "bar"}, {
   success: function(object) {
-    alert("yay! it worked");
+    //alert("yay! it worked");
   }
 });
 
